cfd7bdb10282454ac822496368dbf35c540eb8c9
devengurule <dgurule3@uccs.edu>
2026-03-15 12:38:27 -0600

    Added Node Hover Script

--- Transluminal/Assets/Scripts/Navigation/HoverScript.cs
+++ Transluminal/Assets/Scripts/Navigation/HoverScript.cs
@@ -2,15 +2,75 @@ using UnityEngine;
 
 public class HoverScript : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    
+    #region Variables
+
+    private EventManager eventManager;
+    private bool isHovered;
+    private bool isSelected;
+
+    #endregion
+
+    #region Unity Methods
+    private void Start()
     {
-        
+        eventManager = GameController.instance.eventManager;
+
+        if (eventManager != null)
+        {
+            eventManager.Subscribe(EventType.Interact, OnGameInteract);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnDestroy()
     {
-        
+        if (eventManager != null)
+        {
+            eventManager.Unsubscribe(EventType.Interact, OnGameInteract);
+        }
+    }
+
+    #endregion
+
+    private void OnGameInteract(object target)
+    {
+        if (isHovered)
+        {
+            if (!isSelected)
+            {
+                GameObject selectObject = transform.Find("Selected").gameObject;
+                selectObject.SetActive(true);
+                isSelected = true;
+            }
+            else if (isSelected)
+            {
+                GameObject selectObject = transform.Find("Selected").gameObject;
+                selectObject.SetActive(false);
+                isSelected = false;
+            }
+        }
+    }
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "NavSelector")
+        {
+            if (!isSelected)
+            {
+                GameObject hoverObject = transform.Find("Hovered").gameObject;
+                hoverObject.SetActive(true);
+            }
+            isHovered = true;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "NavSelector")
+        {
+            GameObject hoverObject = transform.Find("Hovered").gameObject;
+            hoverObject.SetActive(false);
+            isHovered = false;
+        }
     }
 }
